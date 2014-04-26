2011c2c169c20596a025606dce2ca4e970052282
Derrick Lockwood <lockwood.derrick@gmail.com>
2014-04-26 17:12:11 -0500

    Added home.html, added some backbone js, bare bones home

---
+++ PPF/public_html/js/mainJS.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function changePage(page)
+{
+    window.location.replace(page);
+    document.location.href = page;
+}
