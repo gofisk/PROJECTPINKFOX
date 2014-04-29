c5bf690e6f24827526b85bc258c23eca24d65d5f
Derrick Lockwood <lockwood.derrick@gmail.com>
2014-04-28 23:53:06 -0500

    Fixed profile buttons for log out and home Messed around with php for upcoming database implementation

---
+++ PPF/public_html/php/login.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["username"]; ?><br>
+Password: <?php echo $_POST["password"]; ?>
+
+</body>
+</html>
---
+++ PPF/public_html/php/welcome.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="login.php" method="post">
+Username: <input type="text" name="username"><br>
+Password: <input type="password" name="password"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
